6e6403af2209469d578b9ea3d7c53be648131c4c
sufyan <mdsufyan7@gmail.com>
2023-05-26 10:39:31 +0530

    add using html form

--- app1/views.py
+++ app1/views.py
@@ -3,10 +3,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-	# return HttpResponse("<h1>Hello world<h1>")
-	return render(request, "home.html")
-
-
+	x = request.GET.get('x','0'),
+	y = request.GET.get('y','0'),
+	z = int(x[0]) + int(y[0])
+	
+	data = {
+		"age":20,
+		"result" : z,
+		"x" : x[0],
+		"y" : y[0],
+	}
+	return render(request,"home.html",data)
 
 def about(request):
 	# return HttpResponse("<h1>About us<h1>")
